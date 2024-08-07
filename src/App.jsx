949b68a1fc8f916529544fb24167ae1fe5e499a4
Rohan B. <rbhole237@gmail.com>
2024-08-06 22:51:18 -0400

    Created folders for my proposed components and some files to start with

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+//Import each of the individual pages
+import Body from "./Components/AboutMe/Body.jsx"
+//import Projects from "./Components/Projects/Projects.jsx"
+//import Spotify from "./Components/Music/Spotify.jsx"
+//import Games from "./Components/Games/Games.jsx"
+
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div className = "Website">
+        
+      <Nameplate />
+      <Body />
+      <Projects />
+      <Music />
+      <Games />
+        
+    </div>
   )
 }
 
---
+++ src/Components/AboutMe/Body.jsx
@@ -0,0 +1,16 @@
+import './Body.css'
+
+const Body = () => {
+
+    return (
+        <div className="About Me">
+            <p className="Biography">
+                Hey all, my name is Rohan, and I'm a CS major from Georgia Tech <br/><br/>
+                My threads (specializations) are People and Media. The People thread focuses on the psychology of people and the Media thread focuses on displaying data through computational means. Combined, these two provide the basis for studying human-computer interaction. <br/><br/>
+                I have some experience coding in <br/><br/>
+            </p>
+        </div>
+    );
+};
+
+export default Body;
